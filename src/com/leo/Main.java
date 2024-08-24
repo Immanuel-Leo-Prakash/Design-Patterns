2f6e9d13217dd2996893674908f29e35c90be0cf
Immanuel Leo Prakash <immanuel.sm@zohocorp.com>
2024-08-24 21:30:08 +0530

    Interface revisit

--- src/com/leo/Main.java
+++ src/com/leo/Main.java
@@ -2,7 +2,11 @@ package com.leo;
 
 public class Main {
     public static void main(String[] args) {
-        User user = new User("Leo");
-        user.sayHello();
+        TaxCalculator calculator = getCalculator();
+        calculator.calculateTax();
+    }
+
+    public static TaxCalculator getCalculator(){
+        return new TaxCalculator2019(); // This will also trigger the recompilation if TaxCalculator2019 changes. Could be resolved using Dependency Insection.
     }
 }
---
+++ src/com/leo/TaxCalculator.java
@@ -0,0 +1,5 @@
+package com.leo;
+
+public interface TaxCalculator {
+    float calculateTax();
+}
