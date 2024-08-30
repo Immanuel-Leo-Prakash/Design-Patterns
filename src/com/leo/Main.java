11c370f7139b8ef9d29e9c28f0d6317d6a9c5896
Immanuel Leo Prakash <immanuel.sm@zohocorp.com>
2024-08-30 21:34:07 +0530

    Memennto pattern hands-on

--- src/com/leo/Main.java
+++ src/com/leo/Main.java
@@ -1,12 +1,20 @@
 package com.leo;
 
+import com.leo.memento.Editor;
+import com.leo.memento.History;
+
+import java.net.SocketOption;
+
 public class Main {
     public static void main(String[] args) {
-        TaxCalculator calculator = getCalculator();
-        calculator.calculateTax();
-    }
-
-    public static TaxCalculator getCalculator(){
-        return new TaxCalculator2019(); // This will also trigger the recompilation if TaxCalculator2019 changes. Could be resolved using Dependency Insection.
+       var editor = new Editor();
+       var history = new History();
+       editor.setContent("0");
+       history.push(editor.createState());
+       editor.setContent("1");
+       history.push(editor.createState());
+       editor.setContent("2");
+       editor.restore(history.pop());
+       System.out.println(editor.getContent());
     }
 }
---
+++ src/com/leo/memento/History.java
@@ -0,0 +1,16 @@
+package com.leo.memento;
+import java.util.List;
+import java.util.ArrayList;
+
+public class History {
+    List<EditorState> states = new ArrayList<>();
+    public void push(EditorState state){
+        states.add(state);
+    }
+    public EditorState pop(){
+        var lastIndex = states.size()-1;
+        var lastState = states.get(lastIndex);
+        states.remove(lastState);
+        return lastState;
+    }
+}
